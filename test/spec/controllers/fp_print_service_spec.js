b596522395344db9066de6314fe31b58c01eea9e
Kiran <kirang20@gmail.com>
2013-07-26 12:24:35 +0530

    Kumaran & Kiran | #460 | Added even more data to fp print register.

--- test/spec/controllers/fp_print_service_spec.js
+++ test/spec/controllers/fp_print_service_spec.js
@@ -10,6 +10,156 @@ describe('Service: FPPrintRegisterService', function () {
     }));
 
     it('should return empty fpUsers', function () {
+        var allECs = [
+            {
+                "registrationDate": "2013-03-02",
+                "ecNumber": "2",
+                "wifeName": "Rani",
+                "husbandName": "Raja",
+                "village": "Keelanapura",
+                "wifeDOB": "1984-08-01",
+                "husbandDOB": "1983-01-01",
+                "caste": "sc",
+                "religion": "hindu",
+                "wifeEducationLevel": "illiterate",
+                "husbandEducationLevel": "illiterate",
+                "lmp": "",
+                "upt": "-ve",
+                "maleChildren": "0",
+                "femaleChildren": "1",
+                "fpDetails": {
+                    "method": "iud",
+                    "details": {
+                        "iudDateOfInsertion": "2013-04-01",
+                        "iudPlaceOfInsertion": "phc",
+                        "iudInserterName": "bhagya",
+                        "remarks": ""
+                    }
+                },
+                "wifeAge": 29
+            },
+            {
+                "registrationDate": "2013-01-01",
+                "ecNumber": "1",
+                "wifeName": "Maanu",
+                "husbandName": "Putta",
+                "village": "Bherya",
+                "wifeDOB": "1987-09-17",
+                "husbandDOB": "1985-12-20",
+                "caste": "c_others",
+                "religion": "hindu",
+                "wifeEducationLevel": "graduate",
+                "husbandEducationLevel": "graduate",
+                "lmp": "",
+                "upt": "-ve",
+                "maleChildren": "0",
+                "femaleChildren": "0",
+                "fpDetails": {
+                    "method": "condom",
+                    "details": {
+                        "refillDates": [
+                            "2013-01-03",
+                            "2013-02-05"
+                        ],
+                        "remarks": "Need to supply Condom to this couple"
+                    }
+                },
+                "wifeAge": 25
+            },
+            {
+                "registrationDate": "2013-02-01",
+                "ecNumber": "34",
+                "wifeName": "Gayathri",
+                "husbandName": "Raju",
+                "village": "Gauribidanur",
+                "wifeDOB": "1980-01-03",
+                "husbandDOB": "1975-04-02",
+                "caste": "",
+                "religion": "hindu",
+                "wifeEducationLevel": "",
+                "husbandEducationLevel": "",
+                "lmp": "",
+                "upt": "",
+                "maleChildren": "",
+                "femaleChildren": "",
+                "fpDetails": {
+                    "method": "ocp",
+                    "details": {
+                        "refillDates": [
+                            "2013-02-01",
+                            "2013-02-04"
+                        ],
+                        "remarks": ""
+                    }
+                },
+                "wifeAge": 33
+            },
+            {
+                "registrationDate": "2013-04-02",
+                "ecNumber": "54",
+                "wifeName": "Fiza",
+                "husbandName": "Farooq",
+                "village": "Dammatmari",
+                "wifeDOB": "1982-01-01",
+                "husbandDOB": "1981-01-02",
+                "caste": "",
+                "religion": "muslim",
+                "wifeEducationLevel": "",
+                "husbandEducationLevel": "",
+                "lmp": "",
+                "upt": "",
+                "maleChildren": "",
+                "femaleChildren": "",
+                "fpDetails": {
+                    "method": "male_sterilization",
+                    "details": {
+                        "doctorName": "krishna murthy",
+                        "typeOfSterilization": "nsv",
+                        "sterilizationDate": "2013-05-01",
+                        "followupVisitDates": [
+                            "2013-05-06",
+                            "2013-05-10",
+                            "2013-06-10"
+                        ],
+                        "remarks": ""
+                    }
+                },
+                "wifeAge": 31
+            },
+            {
+                "registrationDate": "2013-05-12",
+                "ecNumber": "543",
+                "wifeName": "Rangamma",
+                "husbandName": "Chandru",
+                "village": "Bychapura",
+                "wifeDOB": "1986-03-01",
+                "husbandDOB": "1984-03-04",
+                "caste": "",
+                "religion": "jain",
+                "wifeEducationLevel": "",
+                "husbandEducationLevel": "",
+                "lmp": "",
+                "upt": "",
+                "maleChildren": "",
+                "femaleChildren": "",
+                "fpDetails": {
+                    "method": "female_sterilization",
+                    "details": {
+                        "doctorName": "bhyrappa",
+                        "typeOfSterilization": "",
+                        "sterilizationDate": "2013-06-02",
+                        "followupVisitDates": [
+                            "2013-06-06",
+                            "2013-06-10",
+                            "2013-07-10"
+                        ],
+                        "remarks": ""
+                    }
+                },
+                "wifeAge": 27
+            }
+        ];
+
         var expectedFPUsers = {
             iudUsers: [
                 {
@@ -170,6 +320,6 @@ describe('Service: FPPrintRegisterService', function () {
             ]
         };
 
-        expect(fpPrintRegisterService.fpUsers()).toEqual(expectedFPUsers);
+        expect(fpPrintRegisterService.fpUsers(allECs)).toEqual(expectedFPUsers);
     });
 });
