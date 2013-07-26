ac1824c8beb91f50eec791569b996dc4e2a515ee
Kiran <kirang20@gmail.com>
2013-07-26 11:52:58 +0530

    Kumaran & Kiran | #460 | Adding more data to fp print register.

--- app/scripts/fp_print_service.js
+++ app/scripts/fp_print_service.js
@@ -15,7 +15,7 @@ angular.module('drishtiSiteApp')
                 "religion": "hindu",
                 "wifeEducationLevel": "graduate",
                 "husbandEducationLevel": "graduate",
-                "lmp": "",
+                "lmp": "2012-12-15",
                 "upt": "-ve",
                 "maleChildren": "0",
                 "femaleChildren": "0",
@@ -23,11 +23,70 @@ angular.module('drishtiSiteApp')
                     "method": "condom",
                     "details": {
                         "refillDates": [
-                            "2013-01-03",
-                            "2013-02-05",
-                            "","","","","","","","","",""
+                            "2013-04-03",
+                            "2013-05-05",
+                            "", "2013-07-07", "2013-08-15", "", "", "2013-11-20", "", "", "", ""
                         ],
                         "remarks": "Need to supply Condom to this couple"
+                    }
+                }
+            },
+            {
+                "registrationDate": "2013-02-01",
+                "ecNumber": "12",
+                "wifeName": "Pavani",
+                "husbandName": "Pavan",
+                "village": "T Narasipura",
+                "wifeDOB": "1990-01-14",
+                "husbandDOB": "1986-08-13",
+                "caste": "c_others",
+                "religion": "hindu",
+                "wifeEducationLevel": "graduate",
+                "husbandEducationLevel": "graduate",
+                "lmp": "2013-01-27",
+                "upt": "-ve",
+                "maleChildren": "0",
+                "femaleChildren": "0",
+                "fpDetails": {
+                    "method": "condom",
+                    "details": {
+                        "refillDates": [
+                            "2013-04-19",
+                            "2013-05-04",
+                            "2013-06-05",
+                            "2013-07-06",
+                            "", "2013-09-20", "", "2013-10-10", "", "", "2013-02-01", "2013-03-17"
+                        ],
+                        "remarks": "Have to meet them during village visit next week"
+                    }
+                }
+            },
+            {
+                "registrationDate": "2013-02-26",
+                "ecNumber": "12",
+                "wifeName": "Ahalya",
+                "husbandName": "Pandu",
+                "village": "Bannur",
+                "wifeDOB": "1988-03-11",
+                "husbandDOB": "1985-07-11",
+                "caste": "sc",
+                "religion": "hindu",
+                "wifeEducationLevel": "illeterate",
+                "husbandEducationLevel": "puc",
+                "lmp": "2013-02-10",
+                "upt": "-ve",
+                "maleChildren": "1",
+                "femaleChildren": "0",
+                "fpDetails": {
+                    "method": "condom",
+                    "details": {
+                        "refillDates": [
+                            "2013-04-01",
+                            "2013-05-06",
+                            "2013-06-02",
+                            "2013-07-15",
+                            "2013-08-11", "", "2013-10-22", "2013-11-01", "2013-12-20", "", "2013-2-01", "2013-02-17"
+                        ]
                     }
                 }
             },
@@ -53,7 +112,59 @@ angular.module('drishtiSiteApp')
                         "iudDateOfInsertion": "2013-04-01",
                         "iudPlaceOfInsertion": "phc",
                         "iudInserterName": "bhagya",
-                        "remarks": ""
+                        "remarks": "Have to meet them during village visit next week"
+                    }
+                }
+            },
+            {
+                "registrationDate": "2013-04-01",
+                "ecNumber": "245",
+                "wifeName": "baby",
+                "husbandName": "Anthony",
+                "village": "bherya",
+                "wifeDOB": "1982-06-01",
+                "husbandDOB": "1978-08-01",
+                "caste": "c_others",
+                "religion": "christian",
+                "wifeEducationLevel": "graduate",
+                "husbandEducationLevel": "post graduate",
+                "lmp": "2013-02-01",
+                "upt": "-ve",
+                "maleChildren": "1",
+                "femaleChildren": "1",
+                "fpDetails": {
+                    "method": "iud",
+                    "details": {
+                        "iudDateOfInsertion": "2012-03-01",
+                        "iudPlaceOfInsertion": "subcenter",
+                        "iudInserterName": "Dr Rangachari",
+                        "remarks": "Follow up in 2 months"
+                    }
+                }
+            },
+            {
+                "registrationDate": "2013-04-10",
+                "ecNumber": "25",
+                "wifeName": "Fathima",
+                "husbandName": "Abdullaha",
+                "village": "bherya",
+                "wifeDOB": "1985-02-23",
+                "husbandDOB": "1977-12-28",
+                "caste": "",
+                "religion": "muslim",
+                "wifeEducationLevel": "illeterate",
+                "husbandEducationLevel": "sslc",
+                "lmp": "2013-03-18",
+                "upt": "-ve",
+                "maleChildren": "0",
+                "femaleChildren": "1",
+                "fpDetails": {
+                    "method": "iud",
+                    "details": {
+                        "iudDateOfInsertion": "2013-02-23",
+                        "iudPlaceOfInsertion": "district hospital",
+                        "iudInserterName": "LHV Latha",
+                        "remarks": "Follow up in 1 months"
                     }
                 }
             },
@@ -79,7 +190,7 @@ angular.module('drishtiSiteApp')
                         "refillDates": [
                             "2013-02-01",
                             "2013-02-04",
-                            "","","","","","","","","",""
+                            "", "", "", "", "", "", "", "", "", ""
                         ],
                         "remarks": ""
                     }
