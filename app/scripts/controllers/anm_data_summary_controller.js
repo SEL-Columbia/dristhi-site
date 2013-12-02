c1b7cb6475a08982aa0e3f97005a46b86a836440
sowmyas <sm.sowmya14@gmail.com>
2013-12-02 11:20:54 +0530

    Sowmya & Kiran | #759 | Removed anm data summary service and moved the http request logic to controller.

--- app/scripts/controllers/anm_data_summary_controller.js
+++ app/scripts/controllers/anm_data_summary_controller.js
@@ -1,14 +1,39 @@
 'use strict';
 
 angular.module('drishtiSiteApp')
-    .controller('ANMDataSummaryCtrl', function ($scope, ANMDataSummaryService, $http) {
-        ANMDataSummaryService.anms($scope, $http);
+    .controller('ANMDataSummaryCtrl', function ($scope, $http) {
+        var getANMs = function () {
+            var url = 'http://localhost:4567/anms';
+            $http({method: 'GET', url: url}).success(function (data) {
+                $scope.anms = data;
+                return $scope.anms;
+            }).error(function (data, status, headers, config) {
+                    $scope.error = true;
+                });
+        };
+
+        var getXLS = function (anm) {
+            return 'http://localhost:4567/anms/' + anm.username + '/excelreport';
+        };
+
+        var getJSONReportForANM = function (anm) {
+            var url = 'http://localhost:4567/anms/' + anm.username + '/jsonReport';
+            var http = $http({method: 'GET', url: url});
+            http.success(function (data) {
+                $scope.jsonReportForANM = data;
+                return $scope.jsonReportForANM;
+            }).error(function (data, status, headers, config) {
+                    $scope.error = true;
+                });
+        };
+
+        getANMs();
 
         $scope.excelReportsForANM = function (anm) {
-            return ANMDataSummaryService.excelReportsForANM($scope, $http, anm);
+            return getXLS(anm);
         };
 
         $scope.jsonReportsForANM = function (anm) {
-            return ANMDataSummaryService.jsonReportsForANM($scope, $http, anm);
+            return getJSONReportForANM(anm);
         };
     });
