391aa6375f54e1ab43bdef09be2adf6e3238ebe2
Kiran <kirang20@gmail.com>
2013-12-18 15:48:23 +0530

    Kiran | #790 | Added tests for years and months values in archived reports controller.

--- app/scripts/controllers/archived_reports_modal_controller.js
+++ app/scripts/controllers/archived_reports_modal_controller.js
@@ -3,17 +3,20 @@ angular.module('drishtiSiteApp')
         'use strict';
 
         var allMonths = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+
         $scope.years = _.range(ARCHIVED_REPORTS_START_YEAR, new Date().getFullYear() + 1);
 
         $scope.months = function () {
             var today = new Date();
-            if ($scope.selectedYear.year === today.getFullYear()) {
+            if ($scope.selectedYear.year !== today.getFullYear()) {
+                return allMonths;
+            } else {
                 return _.filter(allMonths, function (month) {
-                    return allMonths.indexOf(month) <= today.getMonth()
-                })
+                    return allMonths.indexOf(month) <= today.getMonth();
+                });
             }
-            return allMonths;
         };
+
         $scope.selectedYear = {};
         $scope.selectedMonth = {};
 
---
+++ test/spec/controllers/archived_reports_modal_controller_spec.js
@@ -0,0 +1,76 @@
+'use strict';
+
+describe('Controller: ', function () {
+
+    var scope, createController;
+
+    beforeEach(module('drishtiSiteApp'));
+    beforeEach(inject(function ($rootScope, $controller) {
+        scope = $rootScope.$new();
+        createController = function () {
+            return $controller(
+                'ArchivedReportsModalCtrl', {
+                    '$scope': scope,
+                    '$modalInstance': null,
+                    ARCHIVED_REPORTS_START_YEAR: 2000
+                });
+        };
+    }));
+
+    describe('Years for which reports can be downloaded', function () {
+        beforeEach(function () {
+            Timecop.install();
+        });
+
+        it('should include only current year when current year is same as start year', function () {
+            Timecop.freeze(Date.parse('2000-01-01'));
+
+            createController();
+
+            expect(scope.years).toEqual([2000]);
+        });
+
+
+        it('should include all years from start year to current year', function () {
+            Timecop.freeze(Date.parse('2004-01-01'));
+
+            createController();
+
+            expect(scope.years).toEqual([2000, 2001, 2002, 2003, 2004]);
+        });
+
+        afterEach(function () {
+            Timecop.returnToPresent();
+            Timecop.uninstall();
+        });
+    });
+
+    describe('Months for which reports can be downloaded', function () {
+        beforeEach(function () {
+            Timecop.install();
+        });
+
+        it('should include all months when selected year is not current year', function () {
+            Timecop.freeze(Date.parse('2000-01-01'));
+            createController();
+            scope.selectedYear = {year: 2001};
+
+            expect(scope.months())
+                .toEqual(['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']);
+        });
+
+        it('should include only past months when selected year is current year', function () {
+            Timecop.freeze(Date.parse('2000-05-01'));
+            createController();
+            scope.selectedYear = {year: 2000};
+
+            expect(scope.months())
+                .toEqual(['Jan', 'Feb', 'Mar', 'Apr', 'May']);
+        });
+
+        afterEach(function () {
+            Timecop.returnToPresent();
+            Timecop.uninstall();
+        });
+    });
+});
