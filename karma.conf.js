8587de7b45c965d7d9bfbe227f2af5e393da0ca5
Kumaran Venkataraman <me@kumaranvram.com>
2014-01-15 10:56:54 +0530

    Kiran & Kumaran | #000 | Updated the karma conf and package json to get all node dependencies locally.

--- karma.conf.js
+++ karma.conf.js
@@ -1,60 +1,27 @@
-// Karma configuration
-
-// base path, that will be used to resolve files and exclude
-basePath = '';
-
-// list of files / patterns to load in the browser
-files = [
-    JASMINE,
-    JASMINE_ADAPTER,
-    'app/bower_components/angular/angular.js',
-    'app/bower_components/angular-*/*.min.js',
-    'app/bower_components/angular-mocks/angular-mocks.js',
-    'app/bower_components/timecop/timecop-*.js',
-    'app/bower_components/underscore/underscore-min.js',
-    'app/bower_components/underscore.string/dist/underscore.string.min.js',
-    'app/scripts/*.js',
-    'app/scripts/**/*.js',
-    'test/spec/**/*.js',
-    'app/bower_components/momentjs/moment.js'
-];
-
-// list of files to exclude
-exclude = [];
-
-// test results reporter to use
-// possible values: dots || progress || growl
-reporters = ['progress'];
-
-// web server port
-port = 8080;
-
-// cli runner port
-runnerPort = 9100;
-
-// enable / disable colors in the output (reporters and logs)
-colors = true;
-
-// level of logging
-// possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
-logLevel = LOG_INFO;
-
-// enable / disable watching file and executing tests whenever any file changes
-autoWatch = false;
-
-// Start these browsers, currently available:
-// - Chrome
-// - ChromeCanary
-// - Firefox
-// - Opera
-// - Safari (only Mac)
-// - PhantomJS
-// - IE (only Windows)
-browsers = ['Chrome'];
-
-// If browser does not capture in given timeout [ms], kill it
-captureTimeout = 5000;
-
-// Continuous Integration mode
-// if true, it capture browsers, run tests and exit
-singleRun = false;
+module.exports = function (config) {
+    config.set({
+            frameworks:['jasmine'],
+            basePath: '',
+            files: [
+                'app/bower_components/angular/angular.js',
+                'app/bower_components/angular-*/*.min.js',
+                'app/bower_components/angular-mocks/angular-mocks.js',
+                'app/bower_components/timecop/timecop-*.js',
+                'app/bower_components/underscore/underscore-min.js',
+                'app/bower_components/underscore.string/dist/underscore.string.min.js',
+                'app/scripts/*.js',
+                'app/scripts/**/*.js',
+                'test/spec/**/*.js',
+                'app/bower_components/moment/moment.js'
+            ],
+            reporters: ['dots'],
+            port: 8080,
+            runnerPort: 9100,
+            colors: true,
+            autoWatch: false,
+//            browsers: ['Chrome'],
+            captureTimeout: 5000,
+            singleRun: false
+        }
+    );
+};
